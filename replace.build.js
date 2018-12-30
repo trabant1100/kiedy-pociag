bd4b75ddf5a016d94831080f63ae3d0d1b5f2239
trabant1100 <trabant1100@outlook.com>
2018-12-30 01:08:15 +0100

    One more deployment fix

--- replace.build.js
+++ replace.build.js
@@ -1,5 +1,5 @@
 var replace = require('replace-in-file');
-var buildVersion = process.argv[2];
+var buildVersion = process.argv[2].replace(/(\r\n\t|\n|\r\t|\r)/gm, "");
 const options = {
   files: 'src/environments/environment.prod.ts',
   from: /timestamp\: \'.*\'/g,
