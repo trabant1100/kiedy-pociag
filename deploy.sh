49c1fd7f8ce95a1b7bf1f98baa49458c2304641c
trabant1100 <trabant1100@outlook.com>
2019-01-02 00:26:14 +0100

    Deploy branch to pages

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 npm run updateBuild -- "$(git log -1 --date=format:'%Y-%m-%dT%H:%M:%S' --pretty=format:%cd)"
 ng build --prod --base-href "https://trabant1100.github.io/kiedy-pociag/"
-angular-cli-ghpages --branch=master --dir=dist/kiedy-pociag --no-silent
+angular-cli-ghpages --dir=dist/kiedy-pociag --no-silent
